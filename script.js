44ee4841a7d1d33973d3e23d8bbeab52fb4e0fd9
ozod akromov <akromovasadbek@gmail.com>
2023-09-17 12:39:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+﻿const all = document.querySelector('.range-block .all')
+const tl = document.querySelector('.range-block .tl')
+const tr = document.querySelector('.range-block .tr')
+const br = document.querySelector('.range-block .br')
+const bl = document.querySelector('.range-block .bl')
+const bw = document.querySelector('.range-block .bw')
+const bc = document.querySelector('.range-block .bc')
+const bgc = document.querySelector('.range-block .bgc')
+const block = document.querySelector('.block')
+const textarea = document.querySelector('textarea')
+const border = document.getElementById('border')
+
+
+// Universal Function
+const radiusText = () => {
+    textarea.innerText = `border-radius: ${tl.value}px ${tr.value}px ${br.value}px ${bl.value}px;`
+    textarea.style.color = 'blue' 
+    textarea.style.fontSize = '15px'
+    textarea.style.textTransform = 'uppercase'
+}
+
+function setBorderRadius() {
+    block.style.border = `${bw.value}px solid ${bc.value}`
+    block.style.background = `${bgc.value}`
+    block.style.borderRadius = `${tl.value}px ${tr.value}px ${br.value}px ${bl.value}px`
+
+    radiusText()
+}
+
+// All Borders
+all.addEventListener('input', function () {
+    block.style.borderRadius = `${all.value}px`
+
+    tl.value = all.value
+    tr.value = all.value
+    bl.value = all.value
+    br.value = all.value
+
+    radiusText()
+})
+
+tl.addEventListener('input', setBorderRadius, radiusText())
+
+// Top Right
+tr.addEventListener('input', setBorderRadius, radiusText)
+
+// Bottom Right
+br.addEventListener('input', setBorderRadius, radiusText())
+
+// Bottom Left
+bl.addEventListener('input', setBorderRadius, radiusText())
+
+// Border Styles
+bw.addEventListener('input', setBorderRadius)
+bc.addEventListener('input', setBorderRadius)
+bgc.addEventListener('input', setBorderRadius)
+
+const borderStyle = () => {
+    block.style.borderStyle = border.value
+    radiusText()
+}
+
+border.addEventListener('input', borderStyle)
